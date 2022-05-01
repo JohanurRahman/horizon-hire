a80e71ff51b493e53afc3b3d8d0208c50619e3c4
Johan <johanur.rahman@outlook.com>
2022-05-01 15:35:51 +0600

    Sign up component basic logic and design added

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
 
-  constructor() { }
+  signUpForm = new FormGroup({
+    firstName: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+  });
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+  }
+
+  submit() {
+    if (this.signUpForm.invalid) {
+      return;
+    }
+
+    const formData = this.signUpForm.value;
+    console.log('FORM DATA: ', formData);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/auth/login']);
   }
 
 }
