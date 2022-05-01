7aaabd6880d66a7ee565e4e9f5782679ef666fa6
Johan <johanur.rahman@outlook.com>
2022-05-01 15:21:18 +0600

    Sign In Design and functionality added

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+/* Material */
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+/* Modules */
+import { AuthRoutingModule } from './auth-routing.module';
+import { DisableControlModule } from '../../directives/disable-control/disable-control.module';
+
+/* Components */
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { AuthRoutingModule } from './auth-routing.module';
-
 
 @NgModule({
   declarations: [
@@ -12,7 +24,16 @@ import { AuthRoutingModule } from './auth-routing.module';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    ReactiveFormsModule,
+
+    AuthRoutingModule,
+    DisableControlModule,
+
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 
--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subject, takeUntil, tap } from 'rxjs';
+
+import { HotToastService } from '@ngneat/hot-toast';
+
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent implements OnInit {
 
-  constructor() { }
+export class SignInComponent implements OnDestroy {
 
-  ngOnInit(): void {
+  private destroy$ = new Subject<void>();
+
+  submitting = false;
+
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, Validators.required)
+  });
+
+  constructor(
+    private router: Router,
+    private toast: HotToastService,
+    private authService: AuthService
+  ) {
+  }
+
+  submit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.submitting = true;
+
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password).pipe(
+      this.toast.observe({
+        success: 'Logged in successfully',
+        loading: 'Logging in...',
+        error: ({ message }) => `${message}`
+      }),
+      tap({
+        next: () => {
+          // Navigate to profile
+        },
+        error: () => {
+          this.submitting = false;
+        }
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/auth/sign-up']);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 
 }
