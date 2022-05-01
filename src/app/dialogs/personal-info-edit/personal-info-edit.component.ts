cf7727a6fc2b57f1e571dc2ab01e3a749ad4fa96
Johan <johanur.rahman@outlook.com>
2022-05-02 03:51:29 +0600

    Personal info update functionality updated

--- src/app/dialogs/personal-info-edit/personal-info-edit.component.ts
+++ src/app/dialogs/personal-info-edit/personal-info-edit.component.ts
@@ -1,10 +1,12 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject, switchMap, takeUntil, tap } from 'rxjs';
+
+import { HotToastService } from '@ngneat/hot-toast';
+
 import { User } from '@models';
 import { UserService } from '../../services/user.service';
-import { HotToastService } from '@ngneat/hot-toast';
-import { Subject, switchMap, takeUntil, tap } from 'rxjs';
 import { ImageUploadService } from '../../services/image-upload.service';
 
 @Component({
@@ -12,6 +14,7 @@ import { ImageUploadService } from '../../services/image-upload.service';
   templateUrl: './personal-info-edit.component.html',
   styleUrls: ['./personal-info-edit.component.scss']
 })
+
 export class PersonalInfoEditComponent implements OnInit, OnDestroy {
 
   personalInfoForm: FormGroup;
@@ -20,7 +23,7 @@ export class PersonalInfoEditComponent implements OnInit, OnDestroy {
 
   private destroy$ = new Subject<void>();
 
-  imgURL: any;
+  imgUrl: string | ArrayBuffer | null;
 
   constructor(
     private fb: FormBuilder,
@@ -32,7 +35,10 @@ export class PersonalInfoEditComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
+    this.imgUrl = this.data?.photoURL || null;
+
     this.personalInfoForm = this.fb.group({
+      photoURL: [ this.data?.photoURL || null ],
       firstName: [ this.data?.firstName || null, Validators.required],
       lastName: [ this.data?.lastName || null, Validators.required],
       title: [ this.data?.title || null ],
@@ -56,18 +62,47 @@ export class PersonalInfoEditComponent implements OnInit, OnDestroy {
       uid: this.data.uid
     };
 
-    this.userService.updateUser(formData).pipe(
+    // If it's an url then only update profile
+    if (!formData.photoURL || typeof formData.photoURL === 'string') {
+      this.updateProfile(formData).pipe(
+        tap(() => {
+          this.dialogRef.close();
+        }),
+        takeUntil(this.destroy$)
+      ).subscribe();
+
+      return;
+    }
+
+    // if photoUrl type if File then upload image
+    this.imageUploadService
+      .uploadImage(formData.photoURL, `images/profile/${this.data.uid}`)
+      .pipe(
+        this.toast.observe({
+          loading: 'Uploading profile image...',
+          success: 'Image uploaded successfully',
+          error: 'There was an error in uploading the image',
+        }),
+        switchMap((url) => {
+          const data = { ...formData, uid: this.data.uid, photoURL: url };
+          return this.updateProfile(data);
+        }),
+        tap(() => {
+          this.dialogRef.close();
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  updateProfile(data) {
+    return this.userService.updateUser(data).pipe(
       this.toast.observe({
-          loading: 'Updating profile...',
-          success: 'Profile updated successfully',
-          error: 'There was an error in updating the profile',
-        }
-      ),
-      tap(() => {
-        this.dialogRef.close();
+        loading: 'Updating profile...',
+        success: 'Profile updated successfully',
+        error: 'There was an error in updating the profile',
       }),
-      takeUntil(this.destroy$)
-    ).subscribe();
+    );
   }
 
   ngOnDestroy() {
@@ -76,34 +111,21 @@ export class PersonalInfoEditComponent implements OnInit, OnDestroy {
 
 
   uploadImage(event) {
-    // const files: FileList = event.target.files;
-    //
-    // if (files.length === 0) {
-    //   return;
-    // }
-    //
-    // const isValidImage = this.validateImageFile(files[0].type);
-    //
-    // if (isValidImage) {
-    //   const reader = new FileReader();
-    //   reader.readAsDataURL(files[0]);
-    //   reader.onload = () => this.imgURL = reader.result;
-    // }
+    const files: FileList = event.target.files;
 
-    this.imageUploadService
-      .uploadImage(event.target.files[0], `images/profile/${this.data.uid}`)
-      .pipe(
-        this.toast.observe({
-          loading: 'Uploading profile image...',
-          success: 'Image uploaded successfully',
-          error: 'There was an error in uploading the image',
-        }),
-        switchMap((photoURL) => {
-          const data = { uid: this.data.uid, photoURL };
-          return this.userService.updateUser(data);
-        })
-      )
-      .subscribe();
+    if (files.length === 0) {
+      return;
+    }
+
+    const isValidImage = this.validateImageFile(files[0].type);
+
+    if (isValidImage) {
+      this.personalInfoForm.controls['photoURL'].setValue(files[0]);
+
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = () => this.imgUrl = reader.result;
+    }
   }
 
   validateImageFile(fileType) {
