6a3a58ed65194d1f83e3111a0993316624f4ce15
Johan <johanur.rahman@outlook.com>
2022-05-01 15:19:52 +0600

    Some extra class added in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '45': '45px'
+      },
+      margin: {
+        '70': '70px'
+      },
+      colors: {
+        'orange': '#ff8c00',
+        'blue': '#5c60f5',
+        'grey': '#525f7f'
+      },
+      maxWidth: {
+        '380': '380px'
+      }
+    }
   },
   plugins: [],
 }
