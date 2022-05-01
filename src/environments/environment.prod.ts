7b03742aff0d8cbab54f59854bca318580fb7377
Johan <johanur.rahman@outlook.com>
2022-05-01 15:06:21 +0600

    Angular firebase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'horizon-hire-c6237',
+    appId: '1:256533383764:web:ed7771a4dca865e9017bc1',
+    storageBucket: 'horizon-hire-c6237.appspot.com',
+    apiKey: 'AIzaSyCOvCkXGtBwiXbFpqmxSWpoF-MCG1Allb8',
+    authDomain: 'horizon-hire-c6237.firebaseapp.com',
+    messagingSenderId: '256533383764',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'horizon-hire-c6237',
+    appId: '1:256533383764:web:ed7771a4dca865e9017bc1',
+    storageBucket: 'horizon-hire-c6237.appspot.com',
+    apiKey: 'AIzaSyCOvCkXGtBwiXbFpqmxSWpoF-MCG1Allb8',
+    authDomain: 'horizon-hire-c6237.firebaseapp.com',
+    messagingSenderId: '256533383764',
+  },
   production: false
 };
 
