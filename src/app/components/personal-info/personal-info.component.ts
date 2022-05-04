37d07bf9dde22070298f8c3eceb96f56394e1f01
Johan <johanur.rahman@outlook.com>
2022-05-04 18:15:53 +0600

    Personal Info view component code refactored

--- src/app/components/personal-info/personal-info.component.ts
+++ src/app/components/personal-info/personal-info.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
 import { PersonalInfoEditComponent } from '../../dialogs/personal-info-edit/personal-info-edit.component';
-import { User } from '@models';
 import { ShareProfileComponent } from '../../dialogs/share-profile/share-profile.component';
+
+import { User } from '@models';
 
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
   styleUrls: ['./personal-info.component.scss']
 })
-export class PersonalInfoComponent implements OnInit {
+
+export class PersonalInfoComponent {
 
   @Input() userInfo: User;
   @Input() editable = true;
@@ -19,15 +23,12 @@ export class PersonalInfoComponent implements OnInit {
 
   constructor(private dialog: MatDialog) { }
 
-  ngOnInit(): void {
-  }
-
   editPersonalInfo() {
     this.personalInfoEditDialogRef = this.dialog.open(PersonalInfoEditComponent, {
       data: { ...this.userInfo },
       width: '500px',
       panelClass: 'dialog-edit',
-      disableClose: false
+      disableClose: true
     });
   }
 
